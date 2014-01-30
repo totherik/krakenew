f541a3b55d449e775395d2e9c8c386eecac8115e
ertoth <ertoth@paypal.com>
2014-01-30 07:56:58 -0800

    makara adaro example

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
 'use strict';
 
-var express = require('express'),
-    kraken = require('kraken.next'),
-    stream = require('./lib/stream'),
-    engine = require('./lib/engine');
-
-
-function views(name, context, cb) {
-    var resolved = context.get('alt');
-    if (resolved) {
-        name = name + '_' + resolved;
-    }
-
-    cb(null, name);
-}
-
-
-engine.viewProvider.set('index', views);
-engine.viewProvider.set('partial', views);
-
-
-//engine.dataProvider.set('index', function (name, context, cb) {
-//    setImmediate(cb.bind(null, null, { name: 'Fred' }));
-//});
-
-engine.dataProvider.set('greeting', function (name, context, cb) {
-    setTimeout(cb.bind(null, null, { name: 'Fred' }, 350));
-});
-
-
-engine.dataProvider.set('partial', function (name, context, cb) {
-    setTimeout(cb.bind(null, null, { name: 'Dave' }), 500);
-});
-
+var i18n = require('makara'),
+    express = require('express'),
+    kraken = require('kraken.next');
 
 
 var app = express();
-stream.apply(app);
-app.on('error', console.error.bind(console));
+
+app.on('start', function () {
+    i18n.create(app, app.kraken.get('i18n'))
+});
+
+app.on('error', function (err) {
+    console.error(err.stack);
+});
+
 app.use(kraken());
 app.listen(8000);
 
--- routes.js
+++ routes.js
@@ -4,6 +4,12 @@
 module.exports = function (app) {
 
     app.get('/', function (req, res) {
+        res.locals.context = res.locals.context || {};
+        res.locals.context.locality = {
+            country: 'US',
+            language: 'en'
+        };
+
         res.render('index', { name: 'world', alt: req.query.alt });
     });
 
